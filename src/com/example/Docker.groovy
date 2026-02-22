452ec27a8262c92b14545f90858fdcf8fef6fd8e
Robinson Ibarra <ribarra764@gmail.com>
2026-02-21 18:12:24 -0800

    feat: no-ref: split build image into 3 methods

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -11,11 +11,17 @@ class Docker implements Serializable {
 
     def buildDockerImage(String imageName) {
         script.echo "Building the docker image..."
+        script.sh "docker build -t $imageName ."
+    }
+
+    def dockerLogin() {
         script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-            script.sh "docker build -t $imageName ."
             script.sh "echo '${script.PASS}' | docker login -u '${script.USER}' --password-stdin"
-            script.sh "docker push $imageName"
         }
+    }
+
+    def dockerPush(String imageName) {
+        script.sh "docker push $imageName"
     }
 
 }
